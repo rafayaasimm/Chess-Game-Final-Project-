ff53300510ee5ce1b66d9325281eddd8fc1b2b80
Abdul rafay <rafayaasimm@gmail.com>
2026-05-10 21:42:27 +0500

    added pawn header and cpp

---
+++ pawn.cpp
@@ -0,0 +1,37 @@
+#include "Pawn.h"
+#include "Board.h"
+
+// Pawn created WHITE ho to P aur BLACK ho to p
+Pawn::Pawn(Color c, int r, int cl): Piece(c, r, cl, c == WHITE ? 'P' : 'p') {
+    hasMoved = false; // abhi nahi chala
+}
+
+// Pawn ka symbol return 
+char Pawn::getSymbol() const {
+    return symbol;
+}
+
+// Pawn ka name return 
+string Pawn::getPieceName() const {
+    return "Pawn";
+}
+
+// check move valid hai
+bool Pawn::isValidMove(Board& b, int toRow, int toCol) {
+
+    // WHITE upar jata hai isliye -1
+    // BLACK neeche jata hai isliye +1
+    int direction = (color == WHITE) ? -1 : 1;
+
+    // kitni rows aur colums move hua
+    int rowDiff = toRow - row;
+    int colDiff = toCol - col;
+
+    // sirf ek step aage chal sakta hai
+    if (colDiff == 0 && rowDiff == direction) {
+        return true;
+    }
+
+    // wrong move
+    return false;
+}
---
+++ pawn.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Piece.h"
+
+// Pawn piece is the smallest piece
+class Pawn : public Piece {
+private:
+    bool hasMoved; // Pawn pehle chala hai ya nai
+
+public:
+    Pawn(Color c, int r, int cl);
+    char getSymbol() const override;
+    string getPieceName() const override;
+    bool isValidMove(Board& b, int toRow, int toCol) override;
+};
