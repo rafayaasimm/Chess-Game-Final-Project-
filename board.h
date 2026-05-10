bbe631efe1994f74859772123f2f71c04aa8c552
Abdul rafay <rafayaasimm@gmail.com>
2026-05-10 21:36:51 +0500

    added board header which rameen will add code into

---
+++ board.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// empty board rn
+// Rameen is mein code add krna
+class Piece;
+
+class Board {
+public:
+    
+    Piece* getPiece(int r, int c) const {
+        return nullptr;
+    }
+};
