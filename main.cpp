49516dea0c19b33283fce2f237c271faa8419f43
Abdul rafay <rafayaasimm@gmail.com>
2026-05-10 21:54:33 +0500

    added a main to test rafay pieces

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Piece.h"
+#include "Pawn.h"
+#include "Rook.h"
+#include "Knight.h"
+using namespace std;
+
+int main() {
+
+    //three pieces made for testing
+    Pawn   p(WHITE, 6, 4); // WHITE Pawn row 6 column 4 par
+    Rook   r(WHITE, 7, 0); // WHITE Rook row 7 column 0 par
+    Knight n(BLACK, 0, 1); // BLACK Knight row 0 column 1 par
+
+   //three pieces added to array
+   // Piece* use kiya hai yeh polymorphism hai
+    Piece* pieces[3] = { &p, &r, &n };
+
+    //har piece ki information print test
+    for (int i = 0; i < 3; i++) {
+        cout << pieces[i]->getPieceName()
+             << " -- Symbol: " << pieces[i]->getSymbol()
+             << " -- Color: "
+             << (pieces[i]->getColor() == WHITE ? "WHITE" : "BLACK")
+             << endl;
+    }
+
+    return 0;
+}
