f954245e6854ab4050ad0e4f0b1b0bb0520285b5
Abdul rafay <rafayaasimm@gmail.com>
2026-05-10 21:46:01 +0500

    added rook piece header and cpp

---
+++ rook.cpp
@@ -0,0 +1,29 @@
+#include "Rook.h"
+#include "Board.h"
+
+// Rook banana - WHITE ho to R aur BLACK ho to r
+Rook::Rook(Color c, int r, int cl): Piece(c, r, cl, c == WHITE ? 'R' : 'r') {
+}
+
+// Rook ka symbol return karna
+char Rook::getSymbol() const {
+    return symbol;
+}
+
+// Rook ka name return karna
+string Rook::getPieceName() const {
+    return "Rook";
+}
+
+// check if move is valid
+bool Rook::isValidMove(Board& b, int toRow, int toCol) {
+
+    // Rook sirf seedhi line mein chalta hai
+    // ya toh row same rehti hai ya column same rehta hai
+    if (row == toRow || col == toCol) {
+        return true; // sahi chaal hai
+    }
+
+    // in case not valid
+    return false;
+}
---
+++ rook.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Piece.h"
+
+// Rook piece moves in straight line
+class Rook : public Piece {
+public:
+    Rook(Color c, int r, int cl);
+    char getSymbol() const override;
+    string getPieceName() const override;
+    bool isValidMove(Board& b, int toRow, int toCol) override;
+};
