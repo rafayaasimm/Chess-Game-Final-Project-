a2071e96d3dfdae6283538e3dd63700fed790f99
faryalfatima6 <faryalfatima0601@gmail.com>
2026-05-12 00:19:25 +0500

    implemented isCheckmate and full game loop

--- game.cpp
+++ game.cpp
@@ -38,4 +38,71 @@ Game::Game(string p1Name, string p2Name)
     board.setupBoard();
 }
 
-
+bool Game::isCheckmate(Color color)
+ {
+    for (int r = 0; r < 8; r++)
+        for (int c = 0; c < 8; c++) 
+    {
+            Piece* p = board.getPiece(r, c);
+            if (p == nullptr || p->getColor() != color) 
+            {
+                continue;
+            }
+            for (int tr = 0; tr < 8; tr++)
+                for (int tc = 0; tc < 8; tc++)
+                    if (p->isValidMove(board, tr, tc))     
+                    {
+                        return false;  
+                    }
+        }
+    return true;
+}
+ 
+void Game::start()
+{
+    board.displayBoard();
+    while (true) 
+    {
+        Player& current = players[currentTurn];
+        cout << "\n" << current.getName()
+             << " (" << (current.getColor() == WHITE ? "WHITE" : "BLACK")
+             << ") -- Enter move (e.g. e2 e4): ";
+        string from, to;
+        cin >> from >> to;
+        int fR, fC, tR, tC;
+        if (!parseInput(from, fR, fC) || !parseInput(to, tR, tC))
+         {
+            cout << "Invalid input format. Use letter+number like e2." << endl;
+            continue;
+        }
+        Piece* p = board.getPiece(fR, fC);
+        if (p == nullptr) 
+        {
+            cout << "No piece at " << from << "." << endl;
+            continue;
+        }
+        if (p->getColor() != current.getColor()) 
+        {
+            cout << "That is not your piece!" << endl;
+            continue;
+        }
+        if (!board.movePiece(fR, fC, tR, tC))
+       { 
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+ 
+        board.displayBoard();
+        Color opponent = (current.getColor() == WHITE) ? BLACK : WHITE;
+        if (board.isInCheck(opponent))
+         {
+            if (isCheckmate(opponent))
+             {
+                cout << "\nCHECKMATE! " << current.getName() << " WINS!" << endl;
+                break;
+            }
+            cout << "CHECK!" << endl;
+        }
+        switchTurn();
+    }
+}    
