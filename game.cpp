9f41050c72d85e32c5873b28251d25d3cb4ced20
faryalfatima6 <faryalfatima0601@gmail.com>
2026-05-12 00:18:34 +0500

    Implemented game class

--- game.cpp
+++ game.cpp
@@ -1,5 +1,6 @@
 #include "game.h"
 
+
 Player::Player(string n, Color c)
     : name(n), color(c) {}
  
@@ -12,3 +13,29 @@ Color  Player::getColor() const
      return color; 
     }
  
+
+
+//  GAME Implementations
+
+void Game::switchTurn()
+{
+    currentTurn = 1 - currentTurn;
+}
+ 
+bool Game::parseInput(string in, int& r, int& c) {
+    if (in.length() != 2) 
+    {
+        return false;
+    }
+    c = in[0] - 'a';        
+    r = 8 - (in[1] - '0'); 
+    return (r >= 0 && r < 8 && c >= 0 && c < 8);
+}
+ 
+Game::Game(string p1Name, string p2Name)
+    : players{{p1Name, WHITE}, {p2Name, BLACK}}, currentTurn(0)
+{
+    board.setupBoard();
+}
+
+
